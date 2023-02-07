2fa3a887d0df3ce1dc24404aab6189cdc4081aa2
afif.fatakhul <fatakhulafi11@gmail.com>
2023-02-07 08:08:33 +0700

    Merge

--- pkg/jadwal/jadwal.go
+++ pkg/jadwal/jadwal.go
@@ -2,11 +2,14 @@ package jadwal
 
 import (
 	"backend_presensi_device_address/pkg/common/models"
+	"encoding/csv"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
-	"github.com/xuri/excelize/v2"
 )
 
 type JadwalInput struct {
@@ -51,85 +54,60 @@ func (h handler) ImportJadwal(ctx *gin.Context) {
 
 	ctx.SaveUploadedFile(file, dst)
 
-	// dir, err := os.Getwd()
-	// if err != nil {
-	// 	ctx.JSON(http.StatusInternalServerError, err.Error())
-	// 	return
-	// }
-
-	//fileLocation := filepath.Join(dir, "temp", file.Filename)
-
-	f, err := excelize.OpenFile(dst)
+	dir, err := os.Getwd()
 	if err != nil {
-		fmt.Println(err)
+		ctx.JSON(http.StatusInternalServerError, err.Error())
 		return
 	}
-	defer func() {
-		// Close the spreadsheet.
-		if err := f.Close(); err != nil {
-			fmt.Println(err)
-		}
-	}()
 
-	// Get all the rows in the Sheet1.
-	rows, err := f.GetRows("import_golang")
+	fileLocation := filepath.Join(dir, "temp", file.Filename)
+
+	csvFile, err := os.Open(fileLocation)
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
-	for _, row := range rows {
-		//for _, cell := range row {
-		//fmt.Println(row[0][i])
-		fmt.Print(row[1])
-		//}
-		//fmt.Println()
-	}
-
-	// csvFile, err := os.Open(fileLocation)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
 
 	// Read File into a Variable
-	// lines := csv.NewReader(csvFile)
-	// isFirstRow := true
-	// headerMap := make(map[string]int)
-	// for {
-	// Read row
-	//record, err := lines.Read()
+	lines := csv.NewReader(csvFile)
+	isFirstRow := true
+	headerMap := make(map[string]int)
+	for {
+		//Read row
+		record, err := lines.Read()
 
-	// Stop at EOF.
-	// if err == io.EOF {
-	// 	break
-	// }
+		//Stop at EOF.
+		if err == io.EOF {
+			break
+		}
 
-	// Handle first row case
-	// if isFirstRow {
-	// 	isFirstRow = false
+		//Handle first row case
+		if isFirstRow {
+			isFirstRow = false
 
-	// Add mapping: Column/property name --> record index
-	// for i, v := range record {
-	// 	headerMap[v] = i
-	// }
+			//Add mapping: Column/property name --> record index
+			for i, v := range record {
+				headerMap[v] = i
+			}
 
-	// Skip next code
-	//continue
-	// }
+			//Skip next code
+			continue
+		}
 
-	// fmt.Println(record[headerMap["Tanggal"]])
+		fmt.Println(record[headerMap["Tanggal"]])
 
-	// Create new person and add to persons array
-	//user_id, _ := strconv.ParseInt(record[headerMap["UserID"]], 10, 0)
-	// jadwal := models.Jadwal{Tanggal: record[headerMap["Tanggal"]],
-	// 	JamMulaiMasuk: record[headerMap["JamMulaiMasuk"]], JamAkhirMasuk: record[headerMap["JamAkhirMasuk"]],
-	// 	JamMulaiPulang: record[headerMap["JamMulaiPulang"]], JamAkhirPulang: record[headerMap["JamAkhirPulang"]],
-	// 	UserId: user_id,
-	// }
-	// if err := h.DB.Create(&jadwal).Error; err != nil {
-	// 	ctx.JSON(http.StatusInternalServerError, err)
-	// 	return
-	// }
-	//}
+		// Create new person and add to persons array
+		//user_id, _ := strconv.ParseInt(record[headerMap["UserID"]], 10, 0)
+		// jadwal := models.Jadwal{Tanggal: record[headerMap["Tanggal"]],
+		// 	JamMulaiMasuk: record[headerMap["JamMulaiMasuk"]], JamAkhirMasuk: record[headerMap["JamAkhirMasuk"]],
+		// 	JamMulaiPulang: record[headerMap["JamMulaiPulang"]], JamAkhirPulang: record[headerMap["JamAkhirPulang"]],
+		// 	UserId: user_id,
+		// }
+		// if err := h.DB.Create(&jadwal).Error; err != nil {
+		// 	ctx.JSON(http.StatusInternalServerError, err)
+		// 	return
+		// }
+		//}
+	}
 
 	ctx.JSON(http.StatusCreated, gin.H{"message": "schedule successfull to import"})
 }
